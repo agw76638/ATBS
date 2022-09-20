a7bbcb24b5f6ad33744555dfd0d570690141cd44
agw76638 <agw76638@protonmail.com>
2022-09-20 22:53:01 +0900

    9/20

---
+++ chapter12/2048auto.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+browser = webdriver.Firefox(executable_path=r'C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Python 3.10\\geckodriver.exe')
+browser.get('https://gabrielecirulli.github.io/2048/')
+
+htmlElem = browser.find_element(By.TAG_NAME, 'html')
+while True:
+    htmlElem.send_keys(Keys.UP)
+    htmlElem.send_keys(Keys.RIGHT)
+    htmlElem.send_keys(Keys.DOWN)
+    htmlElem.send_keys(Keys.LEFT)
+
+    # TODO Stop program.
