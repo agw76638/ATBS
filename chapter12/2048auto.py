c8131e4704d74b6116c32f2437082c7b14be3035
agw76638 <agw76638@protonmail.com>
2022-09-24 01:45:30 +0900

    9/23

--- chapter12/2048auto.py
+++ chapter12/2048auto.py
@@ -6,6 +6,9 @@ browser = webdriver.Firefox(executable_path=r'C:\\ProgramData\\Microsoft\\Window
 browser.get('https://gabrielecirulli.github.io/2048/')
 
 htmlElem = browser.find_element(By.TAG_NAME, 'html')
+message = browser.find_element(By.CLASS_NAME, "game-message").text
+over = browser.find_element(By.CLASS_NAME, "game-message")
+score = browser.find_element(By.CLASS_NAME, "score-container").text
 while True:
     htmlElem.send_keys(Keys.UP)
     htmlElem.send_keys(Keys.RIGHT)
@@ -13,3 +16,6 @@ while True:
     htmlElem.send_keys(Keys.LEFT)
 
     # TODO Stop program.
+    if over:
+        print(score)
+        browser.quit()
