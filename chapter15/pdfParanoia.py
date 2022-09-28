511f3dfee4bb06d71da2aa5f20b8d96a02ec24b2
agw76638 <agw76638@protonmail.com>
2022-09-28 23:59:59 +0900

    9/28

--- chapter15/pdfParanoia.py
+++ chapter15/pdfParanoia.py
@@ -1,12 +1,28 @@
 import PyPDF2, os, sys
 
-# Go through folder.
-for foldername, subfolders, filenames in os.walk(folder):
-    for filename in filenames:
-        # TODO Encrypt PDF using a password provided on the command line.
-        if filename.endswith('.pdf'):
-            pdfFile = open(filename, 'rb')
-            pdfWriter = PyPDF2.PdfFileWriter(pdfFile)
-            pdfWriter.encrypt()
+def encryptPdf(folder):
+    folder = os.path.abspath(folder)
+    # Go through folder.
+    for foldername, subfolders, filenames in os.walk(folder):
+        for filename in filenames:
+            # Encrypt PDF using a password provided on the command line.
+            if filename.endswith('.pdf'):
+                pdfFile = open(os.path.join(foldername, filename), 'rb')
+                pdfReader = PyPDF2.PdfFileReader(pdfFile)
+                pdfWriter = PyPDF2.PdfFileWriter()
+                pdfWriter.encrypt(password)
 
-# TODO Save encrypted PDF with an _encrypted.pdf suffix
+                # Save encrypted PDF with an _encrypted.pdf suffix
+                pdfOutput = open(os.path.join(folder, os.path.splitext(filename)[0] + '_encrypted.pdf'), 'wb')
+                pdfWriter.write(pdfOutput)
+                pdfOutput.close()
+
+# TODO
+
+if len(sys.argv) < 2:
+    print('Type password for encryption')
+    sys.exit()
+ 
+password = sys.argv[1]    # first command line arg is the password
+
+encryptPdf('C:\\Users\\ahn\\Downloads\\a')
